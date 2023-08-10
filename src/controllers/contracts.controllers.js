e05dfb03a83f8120eac3545e7d7f5dbd9823895d
Caio Neme <caiocesarneme@gmail.com>
2023-08-10 18:47:05 -0300

    FEAT: get contracts begin

--- src/controllers/contracts.controllers.js
+++ src/controllers/contracts.controllers.js
@@ -21,6 +21,7 @@ export async function postContract(req, res) {
       message: "Não é possivel começar um trabalho antes de assinar o contrato",
     });
   }
+
   try {
     const service = await db.query(`SELECT * FROM services WHERE id = $1;`, [
       id,
@@ -40,14 +41,48 @@ export async function postContract(req, res) {
         .status(403)
         .send({ message: "Esse serviço se encontra indisponivel" });
     }
+
     await db.query(
-      `
-      INSERT INTO contracts ("startDate", "endDate", "userId", "idService") VALUES ($1, $2, $3, $4);
-    `,
+      `INSERT INTO 
+        contracts ("startDate", "endDate", "userId", "idService") 
+      VALUES ($1, $2, $3, $4);`,
       [startDate, endDate, session.rows[0].userId, service.rows[0].id]
     );
 
     res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function getContract(req, res) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+  const { id } = req.params;
+
+  if (!token) {
+    return res.status(401).send({ message: "Falha na autorização" });
+  }
+  if (isNaN(id) || id <= 0) {
+    return res.status(404).send({ message: "URL não encontrada" });
+  }
+
+  try {
+    const session = await db.query(`SELECT * FROM sessions WHERE token = $1`, [
+      token,
+    ]);
+
+    if (session.rowCount != 1) {
+      return res.status(404).send({ message: "serviço não encontrado." });
+    }
+
+    const rajada = await db.query(
+      `SELECT * FROM contracts WHERE "userId" = $1;`,
+      [session.rows[0].userId]
+    );
+    console.log(rajada.rows);
+
+    res.status(200).send("Objeto complexo com altas paradas");
   } catch (error) {
     res.status(500).send(error.message);
   }
--- src/routes/contracts.routes.js
+++ src/routes/contracts.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import validateSchema from "../middlewares/validationSchema.middlewares.js";
-import { postContract } from "../controllers/contracts.controllers.js";
+import {
+  getContract,
+  postContract,
+} from "../controllers/contracts.controllers.js";
 
 const contractRouter = Router();
 
 contractRouter.post("/contract/:id", postContract);
+contractRouter.get("/contract/:id", getContract);
 
 export default contractRouter;
