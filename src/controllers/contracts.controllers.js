d3271be469fef83fc2f5e5d0b755f25a6d4f43d7
Caio Neme <caiocesarneme@gmail.com>
2023-08-10 22:26:28 -0300

    FEAT: update contracts

--- src/controllers/contracts.controllers.js
+++ src/controllers/contracts.controllers.js
@@ -87,3 +87,51 @@ export async function getContract(req, res) {
     res.status(500).send(error.message);
   }
 }
+
+export async function updateContract(req, res) {
+  const { startDate, endDate, status } = req.body;
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+  const { id } = req.params;
+
+  if (!token) {
+    return res.status(401).send({ message: "Falha na autorização" });
+  }
+  if (isNaN(id) || id <= 0) {
+    return res.status(404).send({ message: "URL não encontrada" });
+  }
+
+  try {
+    const session = await db.query(`SELECT * FROM sessions WHERE token = $1;`, [
+      token,
+    ]);
+
+    const contract = await db.query(`SELECT * FROM contracts WHERE id = $1;`, [
+      id,
+    ]);
+
+    console.log(session.rows);
+    console.log(contract.rows);
+
+    if (contract.rows[0].userId != session.rows[0].userId) {
+      return res.status(401).send({ message: "Falha na autorização" });
+    }
+
+    await db.query(
+      `
+      UPDATE contracts 
+      SET 
+        "startDate" = $1, 
+        "endDate" = $2, 
+        status = $3
+      WHERE
+        id = $4
+    ;`,
+      [startDate, endDate, status, id]
+    );
+
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
--- src/routes/contracts.routes.js
+++ src/routes/contracts.routes.js
@@ -3,11 +3,13 @@ import validateSchema from "../middlewares/validationSchema.middlewares.js";
 import {
   getContract,
   postContract,
+  updateContract,
 } from "../controllers/contracts.controllers.js";
 
 const contractRouter = Router();
 
 contractRouter.post("/contract/:id", postContract);
 contractRouter.get("/contract/:id", getContract);
+contractRouter.put("/contract/:id", updateContract);
 
 export default contractRouter;
